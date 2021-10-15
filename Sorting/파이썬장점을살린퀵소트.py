0470e62f6f4f9ed9de02c935331d4b17abe1742e
dmltls94@gmail.com <dmltls94@gmail.com>
2021-10-15 19:36:23 +0900

    sorting/key 정렬, 계수, 일반퀵, 파이썬퀵정렬

---
+++ Sorting/파이썬장점을살린퀵소트.py
@@ -0,0 +1,13 @@
+#파이썬 퀵정렬
+
+array = [5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array):
+  if(len(array) <= 1):
+    return array
+  pivot = array[0]
+  tail = array[1:]
+  left_side = [x for x in tail if pivot>=x]
+  right_side = [x for x in tail if pivot <=x]
+  return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+print(quick_sort(array))
